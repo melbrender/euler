360b6d44815029e9af2f330f8da636de37fde484
Mel Brender <melbrender@aol.com>
2016-02-26 18:24:27 -0500

    Update Problem  36 - double base palindromes.c
    move ispalind() function to eulersubs.c

--- Problem  36 - double base palindromes.c
+++ Problem  36 - double base palindromes.c
@@ -5,7 +5,7 @@
  *
  * Output:
  *
- * 2016-02-23 18:53:21 - CPU time used: 0 hrs 00 mins 00.200 secs
+ * 2016-02-26 18:19:52 - CPU time used: 0 hrs 00 mins 00.180 secs
  *                         there were 20 bipalindromes, adding to 872187
  */
 
@@ -36,35 +36,4 @@ int main(void)
    exit(0);
    }
 
-/* Test a number to see if it is a palindrome in a given base.
- *
- * Form the reverse-reading integer by repeatedly shearing off the lowest
- * order digit and forming a new number with these digits appended on the right.
- * If the new number equals the old number, it was a palindrome.
- *
- * Return values:
- *
- * 1 if the input is a palindrome in base 10
- * 0 otherwise
- */
-int ispalind(int n, int base)
-   {
-   int  i, j, idx=0, digs[100];
-
-   while(n>0)
-      {
-      digs[idx++] = n%base;         /* shear off each digit into an  */
-      n /= base;                    /*  array, starting with the     */
-      }                             /*   lowest order                */
-
-   j = idx/2;
-   for (i=0; i<j; i++)              /* compare each digit            */
-      {                             /*  with it's counterpart in     */
-      if (digs[i] != digs[idx-1-i]) /*   the other direction         */
-         return(0);                 /* if any pair doesn't match     */
-      }                             /*  we're done                   */
-
-   return(1);                       /* if they all matched,          */
-                                    /* it's a palindrome             */
-   }
 
