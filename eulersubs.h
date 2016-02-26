2a3ee8bff7d55f7f6d0d2df78e3e14cf54c57100
Mel Brender <melbrender@aol.com>
2016-02-26 18:28:26 -0500

    Update eulersubs.h
    incorporate ispaling() as common subroutine.

--- eulersubs.h
+++ eulersubs.h
@@ -20,6 +20,7 @@
 int buildprimes(int maxvalue);                        /* build list of primes for isprime() routine   */
 int buildprimesdivs(int maxvalue);                    /* build primes list, divisor counts list       */
 int isprime(int val);                                 /* tests primality; call one of above first     */
+int ispalind(int n, int base);                        /* test if a number is a palindrome in a base   */
 unsigned int comb(unsigned int m, unsigned int n);    /* calculates combinations(m,n)                 */
 int arepermuted(int count, ...);                      /* determine if 2 or more integers have same    */
                                                       /*  digits but possibly permuted                */
